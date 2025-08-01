7bf27ac6204061903bc7543092ead8e5f80353df
Sachith-SL <itsmesachith@gmail.com>
2025-08-01 09:12:55 +0530

    modify rename exercise

--- rename_script.py
+++ rename_script.py
@@ -1,11 +1,16 @@
 import os
 
 image_extensions = ('.jpg', '.jpeg', '.png', '.gif', '.bmp')
+folder_path = './samples_pngs'
 
-image_files = [f for f in os.listdir('.') if f.lower().endswith(image_extensions)]
+image_files = [f for f in os.listdir(folder_path) if f.lower().endswith(image_extensions)]
 
-for index, filename in enumerate(image_files, start=1):
+for index, filename in enumerate(image_files, start=2):
     extension = os.path.splitext(filename)[1]  # Get the file extension
     new_name = f"image_{index}{extension}"
-    os.rename(filename, new_name)
+
+    src = os.path.join(folder_path, filename)   # full source path
+    dst = os.path.join(folder_path, new_name)   # full destination path
+
+    os.rename(src, dst)
     print(f"Renamed: {filename} -> {new_name}")
